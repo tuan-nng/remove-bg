77b9a9660bd1548fea8ed34628e95862391ac804
Yasmin Lopes <yclopes27@gmail.com>
2024-01-03 14:27:24 -0300

    feat: add condition to render button in Historic component

--- app/components/Historic/historic.tsx
+++ app/components/Historic/historic.tsx
@@ -10,13 +10,14 @@ export default function Historic({ listagem, onClick }: Props) {
     <>
       <div className="mt-8">
         <div className="flex gap-4">
+        {listagem.length > 0 && (
         <button
         onClick={onClick}
         className="bg-indigo-600 text-white w-16 h-16 rounded-md flex items-center justify-center transition duration-300 ease-in-out hover:bg-indigo-500"
         title="Remover fundo de outra imagem"
       >
         <Plus/>
-      </button>
+      </button>)}
           {listagem.map((url, index) => (
             <img
               key={index}
