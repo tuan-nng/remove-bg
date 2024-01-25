fe781199c02535c0f74bf1998ef74b854fab5a9f
Yasmin Lopes <yclopes27@gmail.com>
2024-01-25 16:05:48 -0300

    revert 539203e

--- app/page.tsx
+++ app/page.tsx
@@ -25,18 +25,9 @@ export default function Home() {
   const [historico, setHistorico] = useState<string[]>([]);
 
   useEffect(() => {
-    const imagens = localStorage.getItem('imagens');
-
-    if (imagens) {
-      setHistorico(JSON.parse(imagens));
-    }
 
     if (urImagemRemovida) {
-      setHistorico(prevHistorico => {
-        const novoHistorico = [...prevHistorico, urImagemRemovida];
-        localStorage.setItem('imagens', JSON.stringify(novoHistorico));
-        return novoHistorico;
-      });
+      setHistorico(prevHistorico => [...prevHistorico, urImagemRemovida]);
     }
   }, [urImagemRemovida])
 
